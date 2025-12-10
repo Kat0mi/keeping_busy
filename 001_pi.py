bc8b6d22ef13eee00855fa4f555d66cc03183118
Jessica Stewart <n9953523@qut.edu.au>
2025-12-10 10:27:22 +1000

    pi rounder

--- 001_pi.py
+++ 001_pi.py
@@ -1,8 +1,9 @@
 # -- Task --
+
 # Find PI to the Nth Digit - Enter a number and have the program generate PI up to that many decimal places. Limit
 # how far the program will go. 
 
-# -- Initial Logic --
+# -- [10/12/25] --
 
 import numpy as np
 
